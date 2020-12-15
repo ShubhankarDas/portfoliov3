baac76e29412ff21849df0f22d2b0625f84b5605
Shubhankar Das <shubh065@gmail.com>
2020-12-15 16:24:19 +0530

    added loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,14 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
   ],
 }
